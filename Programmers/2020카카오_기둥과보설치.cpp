c667f4f707368d5e23aa745a9888d8f199a998e9
travelbeeee <59816811+travelbeeee@users.noreply.github.com>
2021-03-10 10:47:48 +0900

    Create 2020카카오_기둥과보설치.cpp

---
+++ Programmers/2020카카오_기둥과보설치.cpp
@@ -0,0 +1,82 @@
+/*
+  설치는 문제에서 나온 조건대로 설치가 가능한지 파악하면 된다.
+  canBuildRow := 보 설치가 가능한지
+  canBuildCol := 기둥 설치가 가능한지
+  
+  삭제는 삭제를 하고 나머지 모든 보와 기둥들이 문제에서 나온 설치 조건을 만족하고있는지 체크하면 된다.
+  
+  # 기둥을 삭제할 때는 위에 있는 기둥, 양 옆에 달려있는 보 3가지
+  # 보를 삭제할 때는 양 옆 기둥, 양 옆 보 4가지
+  # 총 7가지만 체크하면 된다고 생각했는데 모든 보와 기둥을 체크하지 않으면 틀렸습니다를 받는다....!
+  
+*/
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool row[105][105] = {}, col[105][105] = {}; // 보, 기둥
+
+void init(void) {
+    for (int i = 0; i < 105; i++)
+        for (int j = 0; j < 105; j++)
+            row[i][j] = col[i][j] = false;
+}
+
+bool canBuildRow(int x, int y) {
+    if (col[x][y - 1] || col[x + 1][y - 1] || (row[x - 1][y] && row[x + 1][y])) return true;
+    return false;
+}
+
+bool canBuildCol(int x, int y) {
+    if (y == 2 || row[x - 1][y] || row[x][y] || col[x][y - 1]) return true;
+    return false;
+}
+
+bool checkAll(int n) {
+    for (int i = 2; i <= n; i++)
+        for (int j = 2; j <= n; j++){
+            if (row[i][j] && !canBuildRow(i, j)) return false;
+            if (col[i][j] && !canBuildCol(i, j)) return false;
+        }
+    return true;
+}
+
+vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
+    init();
+
+    for (int i = 0; i < build_frame.size(); i++) {
+        int x = build_frame[i][0], y = build_frame[i][1], isRow = build_frame[i][2], isBuilt = build_frame[i][3];
+        x += 2, y += 2; // 2 ~ N + 2 까지 사용!
+        if (isBuilt) { //설치
+            if (isRow && canBuildRow(x, y)) // 보 설치
+                    row[x][y] = true;
+            if (!isRow && canBuildCol(x, y))// 기둥 설치
+                    col[x][y] = true;
+        }
+        else { //삭제
+            if (isRow) { // 보 삭제 --> 삭제하고 나머지 다 조사하자
+                row[x][y] = false;
+                if (!checkAll(n)) row[x][y] = true;
+            }
+            else { // 기둥 삭제
+                col[x][y] = false;
+                if (!checkAll(n)) col[x][y] = true;
+            }
+        }
+    }
+    vector<vector<int>> answer;
+    for (int i = 0; i < 105; i++)
+        for (int j = 0; j < 105; j++) {
+            if (row[i][j]) {
+                answer.push_back({ i - 2, j - 2, 1 });
+            }
+            if (col[i][j]) {
+                answer.push_back({ i - 2, j - 2, 0 });
+            }
+        }
+    sort(answer.begin(), answer.end());
+
+    return answer;
+}
