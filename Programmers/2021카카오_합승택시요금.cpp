ed2f40ec020764cd197864816b4b6ef0bb44122f
HyunSeok Lee <59816811+travelbeeee@users.noreply.github.com>
2021-09-01 11:57:52 +0900

    Update 2021카카오_합승택시요금.cpp

--- Programmers/2021카카오_합승택시요금.cpp
+++ Programmers/2021카카오_합승택시요금.cpp
@@ -42,3 +42,38 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
         answer = min(answer, dist[s][i] + dist[i][a] + dist[i][b]);
     return answer;
 }
+
+
+// 다시 푼 풀이
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
+    const int INF = 99999999;
+
+    int cost[201][201];
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            if(i != j) cost[i][j] = INF;
+        }
+    }
+
+    for (vector<int> fare : fares) {
+        cost[fare[0]][fare[1]] = cost[fare[1]][fare[0]] = fare[2];
+    }
+
+    for (int k = 1; k <= n; k++)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                if (cost[i][k] + cost[k][j] < cost[i][j])
+                    cost[i][j] = cost[i][k] + cost[k][j];
+
+    int answer = INF;
+    for (int i = 1; i <= n; i++)
+        answer = min(answer, cost[s][i] + cost[i][a] + cost[i][b]);
+
+    return answer;
+}
