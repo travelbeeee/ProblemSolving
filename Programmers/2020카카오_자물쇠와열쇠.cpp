0222b382481fed54927fcb345b6598d6c1af2dca
HyunSeok Lee <59816811+travelbeeee@users.noreply.github.com>
2021-09-05 14:21:06 +0900

    Update 2020카카오_자물쇠와열쇠.cpp

--- Programmers/2020카카오_자물쇠와열쇠.cpp
+++ Programmers/2020카카오_자물쇠와열쇠.cpp
@@ -57,3 +57,63 @@ bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
     }
     return false;
 }
+
+// 다시 푼 풀이
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void turnClockwise(vector<vector<int>>* key) {
+    int M = key->size();
+
+    vector<vector<int>> tempKey;
+    for (int i = 0; i < M; i++)
+        tempKey.push_back(key->at(i));
+
+    for (int i = 0; i < M; i++)
+        for (int j = 0; j < M; j++)
+            tempKey[i][j] = key->at(M - 1 - j).at(i);
+
+    for (int i = 0; i < M; i++)
+        for (int j = 0; j < M; j++)
+            key->at(i).at(j) = tempKey[i][j];
+}
+
+bool isInside(int x, int y, int N) {
+    return (0 <= x && x < N && 0 <= y && y < N);
+}
+
+bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
+    int M = key.size(), N = lock.size();
+    int goalCnt = 0; // 채워야되는 홈부분
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            goalCnt += 1 - lock[i][j];
+
+    for (int turn = 0; turn < 4; turn++) {
+        //l key를 (i, j)부터 시작해서 놓아본다
+        for (int i = -(M - 1); i <= N - 1; i++) {
+            for (int j = -(M - 1); j <= N - 1; j++) {
+                // key[k][l] 부분이 lock[i + k][j + l] 부분과 만나고 있다
+                int cnt = 0;
+                for (int k = 0; k < M; k++) {
+                    for (int l = 0; l < M; l++) {
+                        if (!isInside(i + k, j + l, N)) continue;
+                        if (key[k][l] == 1 && lock[i + k][j + l] == 1) { // 돌기와 돌기가 만남
+                            k = M, l = M;
+                            break;
+                        }
+                        else if (key[k][l] == 1 && lock[i + k][j + l] == 0) {
+                            cnt++;
+                        }
+                    }
+                }
+                if (cnt == goalCnt)
+                    return true;
+            }
+        }
+        turnClockwise(&key);
+    }
+    return false;
+}
